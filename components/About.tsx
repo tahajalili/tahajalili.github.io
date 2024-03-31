b5474f139d652e83d1ab8f05576b15c2356f79d8
Taha Jalili <tahajalili@gmail.com>
2024-03-31 18:30:08 -0400

    picture and info added

--- components/About.tsx
+++ components/About.tsx
@@ -14,19 +14,23 @@ const About = (): JSX.Element => {
 				<div className="flex flex-col max-w-xl w-full justify-evenly">
 					<h1 className="text-4xl font-bold pb-8 md:pb-0">{personalInfo.name}</h1>
 					<p>
-						I am a {personalInfo.about.year} PhD student
-						in <ExtLink href= {personalInfo.about.department.link}>{personalInfo.about.department.name} </ExtLink>
-						at <ExtLink href= {personalInfo.about.college.link}>{personalInfo.about.college.name} </ExtLink> 
-						advised by <ExtLink href={personalInfo.about.advisor.link}> {personalInfo.about.advisor.name}. </ExtLink>
-						 <br/><a className = "text-sm">✉️ {personalInfo.about.email}</a>
+						I am an undergrad student studying Computer Science
+						at the <ExtLink href= {personalInfo.about.college.link}>{personalInfo.about.college.name.replace(/ /g, "\u00a0")} </ExtLink> 
+						with a passion in cryptography, algorithms, and anything related to theoretical computer science.
+						 
 					</p>
+					<br/>
+					<p>
+						I would like to continue my education with the aim of joining academia as a researcher.
+						I plan to find a graduate position in Computer Science that focuses on my areas of interest
+						once I'm done with my undergraduate studies.
+						<br/><br/><a className = "text-sm">✉️: {personalInfo.about.email}</a>
+					</p>
+
 					<p className = "text-sm text-gray-600">
 						Research Interest: {personalInfo.about.interest}
 					</p>
-					<p className = "text-sm text-gray-400">
-						Under construction......<br/>
-						Re-architecting this site with some modern web technologies.
-					</p>
+					
 				</div>
 			</div>
 		</section>
--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -2,16 +2,19 @@ import Image from 'next/image';
 
 const ProfileImage = (): JSX.Element => {
     
-    return (
-        <Image
-            className="rounded-full select-none transition-all pointer-events-none"
-            src="/images/me.jpg"
-            // onMouseOver = {e => e.currentTarget.src = "/images/gelo.HEIC"}
-            // onMouseOut = {e => e.currentTarget.src = "/images/me.jpg"}
-            draggable={false}
-            alt="My profile image"
-            width={260}
-            height={260}/>
+    return (        
+        <div>
+            <Image
+                className="rounded-full select-none transition-all pointer-events-none"
+                src="/images/me.jpg"
+                // onMouseOver = {e => e.currentTarget.src = "/images/gelo.HEIC"}
+                // onMouseOut = {e => e.currentTarget.src = "/images/me.jpg"}
+                draggable={false}
+                alt="My profile image"
+                width={260}
+                height={260}/>
+
+        </div>
     );
 };
 
